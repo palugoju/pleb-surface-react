9b6a0445df09a54fd3fc1cd26d8f757ded5eac3f
palugoju <praveen08a@gmail.com>
2023-01-20 00:52:24 -0500

    srujana-demo

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h2>Hello javascript reactor on surface book</h2>
+      <h4>Hello srujana, this is web app in Javascript</h4>
       <h3>this is second change</h3>
       <h3>this is for vercel demo</h3>
     </div>

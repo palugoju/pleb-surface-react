c98724d7a5cf771119c957b17ab3844030bdad2c
palugoju <praveen08a@gmail.com>
2023-01-20 00:41:22 -0500

    vercel-demo

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ function App() {
   return (
     <div className="App">
       <h2>Hello javascript reactor on surface book</h2>
-      <h2>this is second change</h2>
+      <h3>this is second change</h3>
+      <h3>this is for vercel demo</h3>
     </div>
   );
 }
